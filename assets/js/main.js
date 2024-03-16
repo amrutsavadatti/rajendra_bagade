8b9b192a1de9841f6a7a937170ea712a689d90d0
Amrut Savadatti <amrutsavadatti@Amruts-iMac.local>
2024-03-16 20:03:07 +0530

    portfolio website

---
+++ assets/js/main.js
@@ -0,0 +1,20 @@
+/*=============== FILTERS TABS ===============*/
+
+
+/*=============== DARK LIGHT THEME ===============*/
+
+
+/*=============== SCROLL REVEAL ANIMATION ===============*/
+const sr = ScrollReveal({
+    origin: 'top',
+    distance: '60px',
+    duration: 2500,
+    delay: 400,
+})
+
+sr.reveal(`.profile__border`)
+sr.reveal(`.profile__name`, {delay: 500})
+sr.reveal(`.profile__profession`, {delay: 600})
+sr.reveal(`.profile__social`, {delay: 700})
+sr.reveal(`.filters__content`, {delay: 800})
+sr.reveal(`.filters`, {delay: 900})
